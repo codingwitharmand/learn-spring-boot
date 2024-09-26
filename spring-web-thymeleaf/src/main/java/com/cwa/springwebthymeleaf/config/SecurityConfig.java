e52c526baaec3bd72d63fefc7fc5eab185b769a7
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2024-09-26 13:47:16 +0200

    setup docker-compose

--- spring-web-thymeleaf/src/main/java/com/cwa/springwebthymeleaf/config/SecurityConfig.java
+++ spring-web-thymeleaf/src/main/java/com/cwa/springwebthymeleaf/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .build();
     }
